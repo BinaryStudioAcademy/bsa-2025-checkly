21853fb6fbbadbbc5eb1d8d7faca85cba3d79dfa
watchlar2000 <sergii.koch@gmail.com>
2025-07-21 22:50:42 +0200

    feat: + enhance loader component with configurable properties cy-12

--- apps/frontend/src/libs/components/loader/loader.tsx
+++ apps/frontend/src/libs/components/loader/loader.tsx
@@ -1,9 +1,28 @@
 import { type JSX } from "react";
 
-const Loader = (): JSX.Element => {
+type LoaderContainer = "fullscreen" | "inline";
+type LoaderSize = "large" | "medium" | "small";
+type LoaderTheme = "accent" | "brand" | "muted";
+
+type Properties = {
+	container?: LoaderContainer;
+	size?: LoaderSize;
+	theme?: LoaderTheme;
+};
+
+const Loader = ({
+	container = "fullscreen",
+	size = "large",
+	theme = "brand",
+}: Properties): JSX.Element => {
 	return (
-		<div>
-			<span>Loading...</span>
+		<div className={`loader loader-${container}`}>
+			<div
+				className={`loader-spinner loader-${theme} loader-${size}`}
+				role="status"
+			>
+				<span className="visually-hidden">Loading in progress...</span>
+			</div>
 		</div>
 	);
 };
