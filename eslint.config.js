7dcfe7c89d4f08f3532ea6b1881e9d6f0b2dbaf4
daniil-tkalenko <81804306+daniil-tkalenko@users.noreply.github.com>
2025-07-22 16:17:55 +0300

    fix: linter cy-0

--- eslint.config.js
+++ eslint.config.js
@@ -139,7 +139,7 @@ const perfectionistConfig = {
 
 /** @type {Config} */
 const typescriptConfig = {
-	ignores: ["eslint.config.js", "lint-staged.config.js", "stylelint.config.js"],
+	ignores: ["eslint.config.js", "lint-staged.config.js", "stylelint.config.js", "prettier.config.js"],
 	languageOptions: {
 		parser: /** @type {ParserModule} */ (tsParser),
 		parserOptions: {
