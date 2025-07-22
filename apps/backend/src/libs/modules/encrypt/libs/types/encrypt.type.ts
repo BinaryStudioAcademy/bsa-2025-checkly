a47caf94aab553adc673c4e78c791757bc06a6c3
astowner <the1samevl@gmail.com>
2025-07-22 16:22:21 +0300

    feat: add encrypt module cy-5

---
+++ apps/backend/src/libs/modules/encrypt/libs/types/encrypt.type.ts
@@ -0,0 +1,6 @@
+type Encrypt = {
+	compare(value: string, storedHash: string, salt: string): Promise<boolean>;
+	encrypt(value: string): Promise<{ hash: string; salt: string }>;
+};
+
+export { type Encrypt };
