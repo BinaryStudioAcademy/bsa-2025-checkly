985f422dbc21f42ebee8025d25a9e08463e819ae
watchlar2000 <sergii.koch@gmail.com>
2025-07-22 21:32:46 +0200

    feat: + add backdrop prop to loader component, create class expressions cy-12

--- apps/frontend/src/libs/components/loader/loader.tsx
+++ apps/frontend/src/libs/components/loader/loader.tsx
@@ -9,6 +9,7 @@ type LoaderSize = "base" | "large" | "medium" | "small";
 type LoaderTheme = "accent" | "brand" | "muted";
 
 type Properties = {
+	backdrop?: boolean;
 	container?: LoaderContainer;
 	size?: LoaderSize;
 	theme?: LoaderTheme;
@@ -16,15 +17,21 @@ type Properties = {
 
 const Loader = ({
 	container = "fullscreen",
+	backdrop = container === "fullscreen",
 	size = "large",
 	theme = "brand",
 }: Properties): JSX.Element => {
+	const containerClasses = cn(styles["loader"], styles[container]);
+	const spinnerClasses = cn(
+		styles["spinner"],
+		styles[size],
+		styles[theme],
+		backdrop && styles["backdrop"],
+	);
+
 	return (
-		<div className={cn(styles["loader"], styles[container])}>
-			<div
-				className={cn(styles["spinner"], styles[theme], styles[size])}
-				role="status"
-			>
+		<div className={containerClasses}>
+			<div className={spinnerClasses} role="status">
 				<span className="visually-hidden">Loading in progress...</span>
 			</div>
 		</div>
