3079b758bb1fe1b1f8c2022dffeec6a714e2a994
Illia Prokopenko <nobiqp@MacSwin-Air.local>
2025-07-22 11:39:25 -0500

    feat: jwt token helper created on frontend cy-7

---
+++ apps/frontend/src/pages/auth/auth-token.helper.ts
@@ -0,0 +1,13 @@
+const storeToken = (token: string): void => {
+    localStorage.setItem("token", token);
+};
+
+const getToken = (): null | string => {
+	return localStorage.getItem("token");
+};
+
+const clearToken = (): void => {
+	localStorage.removeItem("token");
+};
+
+export {clearToken, getToken, storeToken}
