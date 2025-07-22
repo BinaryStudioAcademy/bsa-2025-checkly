696c13f85929471bcbf57b31bc08f9ffde44c1ab
Volin13 <olegkukharishin@gmail.com>
2025-07-22 19:27:36 +0300

    feat(frontend): store user data in state on successful sign-up (#cy-3)

--- apps/frontend/src/modules/auth/slices/auth.slice.ts
+++ apps/frontend/src/modules/auth/slices/auth.slice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import  {type UserSignUpResponseDto } from "shared";
 
 import { DataStatus } from "~/libs/enums/enums.js";
 import { type ValueOf } from "~/libs/types/types.js";
@@ -7,22 +8,27 @@ import { signUp } from "./actions.js";
 
 type State = {
 	dataStatus: ValueOf<typeof DataStatus>;
+	user: null | UserSignUpResponseDto;
 };
 
 const initialState: State = {
 	dataStatus: DataStatus.IDLE,
+	user: null,
 };
 
 const { actions, name, reducer } = createSlice({
 	extraReducers(builder) {
 		builder.addCase(signUp.pending, (state) => {
 			state.dataStatus = DataStatus.PENDING;
+			state.user = null;
 		});
-		builder.addCase(signUp.fulfilled, (state) => {
+		builder.addCase(signUp.fulfilled, (state, action) => {
 			state.dataStatus = DataStatus.FULFILLED;
+			state.user = action.payload;
 		});
 		builder.addCase(signUp.rejected, (state) => {
 			state.dataStatus = DataStatus.REJECTED;
+			state.user = null;
 		});
 	},
 	initialState,
