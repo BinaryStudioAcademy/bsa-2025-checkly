01444eb97a9e1d238a89c6ce5ca77b31f979af6a
astowner <the1samevl@gmail.com>
2025-07-24 15:21:05 +0300

    fix: lint errors cy-5

--- packages/shared/src/modules/users/libs/enums/user-validation-regex-rule.enum.ts
+++ packages/shared/src/modules/users/libs/enums/user-validation-regex-rule.enum.ts
@@ -5,7 +5,7 @@ const UserValidationRegexRule = {
 		`^(?!\\.)((?!\\.{2})[\\w!#$%&'*+=?^\`{|}~./-]){${String(UserValidationRule.EMAIL_LOCAL_MIN_LENGTH)},${String(UserValidationRule.EMAIL_LOCAL_MAX_LENGTH)}}(?<!\\.)@(?![-.])(?=.{${String(UserValidationRule.EMAIL_DOMAIN_MIN_LENGTH)},${String(UserValidationRule.EMAIL_DOMAIN_MAX_LENGTH)}}$)[\\dA-Za-z]+(?:-[\\dA-Za-z]+)*(?:\\.[\\dA-Za-z]+(?:-[\\dA-Za-z]+)*)+(?<![-.])$`,
 	),
 	NAME_VALID_CHARS_MIN_MAX: new RegExp(
-		`^(?=.{${String(UserValidationRule.NAME_MIN_LENGTH)},${String(UserValidationRule.NAME_MAX_LENGTH)}}$)[a-zA-Z]+(?:[\\s-][a-zA-Z]+)*$`
+		`^(?=.{${String(UserValidationRule.NAME_MIN_LENGTH)},${String(UserValidationRule.NAME_MAX_LENGTH)}}$)[a-zA-Z]+(?:[\\s-][a-zA-Z]+)*$`,
 	),
 	PASSWORD_VALID_CHARS_MIN_MAX: new RegExp(
 		`^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[\\S]{${String(UserValidationRule.PASSWORD_MIN_LENGTH)},${String(UserValidationRule.PASSWORD_MAX_LENGTH)}}$`,
