f53cad45e0179757aaac4a5a3bc0dab887baa8d3
watchlar2000 <sergii.koch@gmail.com>
2025-07-29 23:53:23 +0200

    feat: + add icons cy-14

--- apps/frontend/src/assets/img/icons/icons.tsx
+++ apps/frontend/src/assets/img/icons/icons.tsx
@@ -1,2 +1,5 @@
 export { default as EyeClosedIcon } from "./eye-closed.svg?react";
 export { default as EyeOpenIcon } from "./eye-open.svg?react";
+export { default as FileIcon } from "./file.svg?react";
+export { default as MonitorIcon } from "./monitor.svg?react";
+export { default as PhoneIcon } from "./smartphone.svg?react";
