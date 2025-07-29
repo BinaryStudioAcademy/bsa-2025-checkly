b21f7a63184c0ca2772909c5a00609fc4c9b3e15
Kaluha Bohdan <bkdragon232@gmail.com>
2025-07-29 10:55:22 +0300

    fix: refactor notification service cy-45

--- apps/frontend/src/libs/enums/enums.ts
+++ apps/frontend/src/libs/enums/enums.ts
@@ -1,4 +1,3 @@
-export { NotificationOptionValues } from "../modules/notifications/libs/enums/notification-option-values.enum.js";
 export { AppRoute } from "./app-route.enum.js";
 export { DataStatus } from "./data-status.enum.js";
 export { APIPath, AppEnvironment, ContentType, ServerErrorType } from "shared";
--- apps/frontend/src/libs/modules/notifications/notifications.module.ts
+++ apps/frontend/src/libs/modules/notifications/notifications.module.ts
@@ -1,9 +1,9 @@
 import { toast, type ToastOptions, Zoom } from "react-toastify";
 
-import { NotificationOptionValues } from "~/libs/enums/enums.js";
+import { NotificationOptionValues } from "./libs/enums/notification-option-values.enum.js";
 
 class Notifications {
-	private defaultNotificationOptions: Readonly<ToastOptions> = {
+	private static defaultNotificationOptions: Readonly<ToastOptions> = {
 		autoClose: NotificationOptionValues.AUTO_CLOSE_TIME,
 		closeOnClick: false,
 		draggable: true,
@@ -14,25 +14,38 @@ class Notifications {
 		transition: Zoom,
 	};
 
+	private static setOptions(overrides?: Partial<ToastOptions>): ToastOptions {
+		return { ...this.defaultNotificationOptions, ...overrides } as ToastOptions;
+	}
+
 	public error(
 		message: string,
-		notificationOptions: ToastOptions = this.defaultNotificationOptions,
+		notificationOptions?: Partial<ToastOptions>,
 	): string {
-		return toast.error(message, notificationOptions).toString();
+		const customNotificationOptions =
+			Notifications.setOptions(notificationOptions);
+
+		return toast.error(message, customNotificationOptions).toString();
 	}
 
 	public info(
 		message: string,
-		notificationOptions: ToastOptions = this.defaultNotificationOptions,
+		notificationOptions?: Partial<ToastOptions>,
 	): string {
-		return toast.info(message, notificationOptions).toString();
+		const customNotificationOptions =
+			Notifications.setOptions(notificationOptions);
+
+		return toast.info(message, customNotificationOptions).toString();
 	}
 
 	public success(
 		message: string,
-		notificationOptions: ToastOptions = this.defaultNotificationOptions,
+		notificationOptions?: Partial<ToastOptions>,
 	): string {
-		return toast.success(message, notificationOptions).toString();
+		const customNotificationOptions =
+			Notifications.setOptions(notificationOptions);
+
+		return toast.success(message, customNotificationOptions).toString();
 	}
 }
 
