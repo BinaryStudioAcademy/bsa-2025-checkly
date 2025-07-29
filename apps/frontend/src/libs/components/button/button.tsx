3ac6c722c27edb52c76108b85d8b6e0c02218fc8
watchlar2000 <sergii.koch@gmail.com>
2025-07-29 23:57:14 +0200

    feat: + update button component cy-14

--- apps/frontend/src/libs/components/button/button.tsx
+++ apps/frontend/src/libs/components/button/button.tsx
@@ -1,11 +1,38 @@
+import { getClassNames } from "~/libs/helpers/get-class-names.js";
+
+import styles from "./styles.module.css";
+
 type Properties = {
+	disabled?: boolean;
+	icon?: React.ReactNode;
 	label: string;
+	size?: "large" | "small";
 	type?: "button" | "submit";
+	variant?: "primary" | "secondary" | "transparent";
 };
 
 const Button: React.FC<Properties> = ({
+	disabled = false,
+	icon,
 	label,
+	size = "large",
 	type = "button",
-}: Properties) => <button type={type}>{label}</button>;
+	variant = "primary",
+}: Properties) => {
+	const buttonClasses = getClassNames(
+		styles["button"],
+		styles[`button-${variant}`],
+		styles[`button-${size}`],
+		styles["button-cluster"],
+		"cluster",
+	);
+
+	return (
+		<button className={buttonClasses} disabled={disabled} type={type}>
+			{icon && <span className={styles["button-icon"]}>{icon}</span>}
+			<span>{label}</span>
+		</button>
+	);
+};
 
 export { Button };
