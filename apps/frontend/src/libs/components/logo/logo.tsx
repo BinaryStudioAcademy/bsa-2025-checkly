f042f4df06cd42c2a4d8aadcfbbe6b7ce88eb36b
watchlar2000 <sergii.koch@gmail.com>
2025-07-30 22:07:11 +0200

    feat: + add app-route to logo component cy-14

--- apps/frontend/src/libs/components/logo/logo.tsx
+++ apps/frontend/src/libs/components/logo/logo.tsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 
 import { logoIcon } from "~/assets/img/shared/shared.img.js";
+import { AppRoute } from "~/libs/enums/app-route.enum.js";
 import { getClassNames } from "~/libs/helpers/get-class-names.js";
 
 import styles from "./styles.module.css";
 
 const Logo: React.FC = () => {
 	return (
-		<Link className={getClassNames(styles["container"], "cluster")} to="/">
+		<Link
+			className={getClassNames(styles["container"], "cluster")}
+			to={AppRoute.ROOT}
+		>
 			<img alt="Binary Checkly web-application logo" src={logoIcon} />
 			Checkly
 		</Link>
