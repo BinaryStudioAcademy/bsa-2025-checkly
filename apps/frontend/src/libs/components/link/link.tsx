cbb70eb53632cbd8b1169cc8fe80819b21385105
watchlar2000 <sergii.koch@gmail.com>
2025-07-30 12:09:20 +0200

    feat: + add asButton prop to link component cy-14

--- apps/frontend/src/libs/components/link/link.tsx
+++ apps/frontend/src/libs/components/link/link.tsx
@@ -1,15 +1,36 @@
 import { NavLink } from "react-router-dom";
 
+import buttonStyles from "~/libs/components/button/styles.module.css";
 import { type AppRoute } from "~/libs/enums/enums.js";
+import { getClassNames } from "~/libs/helpers/get-class-names.js";
 import { type ValueOf } from "~/libs/types/types.js";
 
 type Properties = {
+	asButton?: boolean;
 	children: React.ReactNode;
 	to: ValueOf<typeof AppRoute>;
 };
 
-const Link: React.FC<Properties> = ({ children, to }: Properties) => (
-	<NavLink to={to}>{children}</NavLink>
-);
+const Link: React.FC<Properties> = ({
+	asButton = false,
+	children,
+	to,
+}: Properties) => {
+	const linkClasses = asButton
+		? getClassNames(
+				buttonStyles["button"],
+				buttonStyles["button-secondary"],
+				buttonStyles["button-small"],
+				buttonStyles["button-cluster"],
+				"cluster",
+			)
+		: "";
+
+	return (
+		<NavLink className={linkClasses} to={to}>
+			{asButton ? children : <span>{children}</span>}
+		</NavLink>
+	);
+};
 
 export { Link };
