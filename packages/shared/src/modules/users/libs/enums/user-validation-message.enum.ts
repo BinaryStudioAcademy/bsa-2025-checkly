ebfe458f78a8d5264fd224539ca0ec5ab0f92adf
astowner <the1samevl@gmail.com>
2025-07-30 18:42:03 +0300

    fix: enhance name field validation error messages in sign-up validation schema cy-71

--- packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
+++ packages/shared/src/modules/users/libs/enums/user-validation-message.enum.ts
@@ -4,7 +4,10 @@ const UserValidationMessage = {
 	EMAIL_ALREADY_EXISTS: "Email already in use",
 	EMAIL_INVALID: "Invalid email format",
 	FIELD_REQUIRED: "Field is required",
-	NAME_INVALID: `Name must be between ${String(UserValidationRule.NAME_MIN_LENGTH)} and ${String(UserValidationRule.NAME_MAX_LENGTH)} characters`,
+	NAME_LENGTH: `Name must have at least ${String(UserValidationRule.NAME_MIN_LENGTH)} characters and maximum of ${String(UserValidationRule.NAME_MAX_LENGTH)} characters`,
+	NAME_ONLY_ALLOWED_CHARS: "Digits and special characters are not allowed",
+	NAME_SURROUNDING_RULE:
+		"Spaces and hyphens are allowed, as long as they are surrounded by letters",
 	PASSWORD_INVALID: `Password should contain between ${String(UserValidationRule.PASSWORD_MIN_LENGTH)} to ${String(UserValidationRule.PASSWORD_MAX_LENGTH)} characters, at least one lowercase letter, one uppercase letter and one digit`,
 	USER_NOT_FOUND: "User not found",
 };
