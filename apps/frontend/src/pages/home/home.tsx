0a2be0e7da25c4c5005a37783f5a679869219590
watchlar2000 <sergii.koch@gmail.com>
2025-07-30 21:52:04 +0200

    feat: + add button demos to home page cy-14

--- apps/frontend/src/pages/home/home.tsx
+++ apps/frontend/src/pages/home/home.tsx
@@ -1,3 +1,6 @@
+import { EyeOpenIcon } from "~/assets/img/icons/icons.js";
+import { Button } from "~/libs/components/components.js";
+
 import { Header, Hero } from "./components/components.js";
 import styles from "./styles.module.css";
 
@@ -7,6 +10,80 @@ const Home: React.FC = () => {
 			<Header />
 			<main>
 				<Hero />
+				<div className="wrapper flow-loose" style={{ "paddingBlock": "2rem" }}>
+					<div className="flow-loose">
+						<h2>Buttons:</h2>
+						<div className="flow-loose">
+							<h4>Default size (large) - all variants</h4>
+							<div className="cluster">
+								<Button label="Primary Button" />
+								<Button disabled label="Primary Disabled" />
+							</div>
+							<div className="cluster">
+								<Button label="Secondary Button" variant="secondary" />
+								<Button
+									disabled
+									label="Secondary Disabled"
+									variant="secondary"
+								/>
+							</div>
+							<div className="cluster">
+								<Button label="Transparent Button" variant="transparent" />
+								<Button
+									disabled
+									label="Transparent Disabled"
+									variant="transparent"
+								/>
+							</div>
+						</div>
+						<div className="flow-loose">
+							<h4>Small size with icons - all variants</h4>
+							<div className="cluster">
+								<Button
+									icon={<EyeOpenIcon />}
+									label="Primary Small"
+									size="small"
+								/>
+								<Button
+									disabled
+									icon={<EyeOpenIcon />}
+									label="Primary Small Disabled"
+									size="small"
+								/>
+							</div>
+							<div className="cluster">
+								<Button
+									icon={<EyeOpenIcon />}
+									label="Secondary Small"
+									size="small"
+									variant="secondary"
+								/>
+								<Button
+									disabled
+									icon={<EyeOpenIcon />}
+									label="Secondary Small Disabled"
+									size="small"
+									variant="secondary"
+								/>
+							</div>
+							<div className="cluster">
+								<Button
+									icon={<EyeOpenIcon />}
+									label="Transparent Small"
+									size="small"
+									variant="transparent"
+								/>
+								<Button
+									disabled
+									icon={<EyeOpenIcon />}
+									label="Transparent Small Disabled"
+									size="small"
+									variant="transparent"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
 				<section className={styles["brand"]} data-section-variant>
 					<div className="wrapper">
 						<h2>How it works</h2>
