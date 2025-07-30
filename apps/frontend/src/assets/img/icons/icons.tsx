268519c6df9ee396a6764be6de22a15e2804cdf7
watchlar2000 <sergii.koch@gmail.com>
2025-07-30 21:53:49 +0200

    feat: - remove unused icon exports cy-14

--- apps/frontend/src/assets/img/icons/icons.tsx
+++ apps/frontend/src/assets/img/icons/icons.tsx
@@ -1,5 +1,2 @@
 export { default as EyeClosedIcon } from "./eye-closed.svg?react";
 export { default as EyeOpenIcon } from "./eye-open.svg?react";
-export { default as FileIcon } from "./file.svg?react";
-export { default as MonitorIcon } from "./monitor.svg?react";
-export { default as PhoneIcon } from "./smartphone.svg?react";
