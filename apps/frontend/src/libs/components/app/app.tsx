3e4d2aa4c23e80540c904e19fba8f8ad5e4034a4
olenkabilonizhka <olenka.bilonizhka@gmail.com>
2025-07-30 14:54:53 +0300

    refactor: revert removed user slice cy-8

--- apps/frontend/src/libs/components/app/app.tsx
+++ apps/frontend/src/libs/components/app/app.tsx
@@ -1,8 +1,31 @@
 import { ToastContainer } from "react-toastify";
 
 import { RouterOutlet } from "~/libs/components/components.js";
+import { AppRoute } from "~/libs/enums/enums.js";
+import {
+	useAppDispatch,
+	useAppSelector,
+	useEffect,
+	useLocation,
+} from "~/libs/hooks/hooks.js";
+import { actions as userActions } from "~/modules/users/users.js";
 
 const App: React.FC = () => {
+	const { pathname } = useLocation();
+	const dispatch = useAppDispatch();
+	useAppSelector(({ users }) => ({
+		dataStatus: users.dataStatus,
+		users: users.users,
+	}));
+
+	const isRoot = pathname === AppRoute.ROOT;
+
+	useEffect(() => {
+		if (isRoot) {
+			void dispatch(userActions.loadAll());
+		}
+	}, [isRoot, dispatch]);
+
 	return (
 		<>
 			<RouterOutlet />
