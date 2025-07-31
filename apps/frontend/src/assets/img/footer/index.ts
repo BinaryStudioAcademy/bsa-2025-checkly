14fab916af91d5ae28b85639ed299ec4f19fa84d
Amicuchi <AndersonAmicuchi@gmail.com>
2025-07-31 09:35:49 -0300

    fix: changing logo path to shared images cy-22

--- apps/frontend/src/assets/img/footer/index.ts
+++ apps/frontend/src/assets/img/footer/index.ts
@@ -1,4 +1,4 @@
-export { default as Logo } from "../sign-in/logo-placeholder.svg?react";
+export { default as Logo } from "../shared/logo-icon.svg?react";
 export { default as FbIcon } from "./fb.svg?react";
 export { default as IgIcon } from "./ig.svg?react";
 export { default as YtIcon } from "./yt.svg?react";
