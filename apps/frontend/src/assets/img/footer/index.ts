b7005282f7c4eea13306d7bae65adc257cf27635
Amicuchi <AndersonAmicuchi@gmail.com>
2025-07-31 06:14:44 -0300

    chore: adapting comments to the projext patterns cy-22

--- apps/frontend/src/assets/img/footer/index.ts
+++ apps/frontend/src/assets/img/footer/index.ts
@@ -1,9 +1,3 @@
-/**
- * @file This barrel file centralizes all SVGs used in the footer.
- *
- * @pattern Barrel Exports Pattern
- */
-
 export { default as Logo } from "../sign-in/logo-placeholder.svg?react";
 export { default as FbIcon } from "./fb.svg?react";
 export { default as IgIcon } from "./ig.svg?react";
