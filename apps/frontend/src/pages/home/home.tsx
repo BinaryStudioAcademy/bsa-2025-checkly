816a7feca8a859ef0e60583b6c7622ab00a63ab3
alex-tuz <alex-tuz@yandex.ru>
2025-07-31 11:35:48 -0500

    chore: fix lint errors cy-21

--- apps/frontend/src/pages/home/home.tsx
+++ apps/frontend/src/pages/home/home.tsx
@@ -9,23 +9,36 @@ const Home: React.FC = () => {
 			<Header />
 			<main className={styles["landing-page"]}>
 				<Hero />
-				<section className={getClassNames(styles["landing-section"], styles["brand"])} data-section-variant>
+				<section
+					className={getClassNames(styles["landing-section"], styles["brand"])}
+					data-section-variant
+				>
 					<div className="wrapper">
 						<h2>How it works</h2>
 					</div>
 				</section>
-				<section className={getClassNames(styles["landing-section"])} data-section-variant>
+				<section
+					className={getClassNames(styles["landing-section"])}
+					data-section-variant
+				>
 					<div className="wrapper">
 						<h2>Categories</h2>
 					</div>
 				</section>
-				<section className={getClassNames("grid-pattern", styles["landing-section"])} data-section-variant="dark">
+				<section
+					className={getClassNames("grid-pattern", styles["landing-section"])}
+					data-section-variant="dark"
+				>
 					<div className="wrapper">
 						<h2>Sample visual layouts</h2>
 					</div>
 				</section>
 				<section
-					className={getClassNames("grid-pattern", styles["landing-section"], styles["light-background"])}
+					className={getClassNames(
+						"grid-pattern",
+						styles["landing-section"],
+						styles["light-background"],
+					)}
 				>
 					<div className="wrapper">
 						<FeedbacksSection />
