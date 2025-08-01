bdf62d6ecb58fb6821d7e806ba2db000e96bf210
alex-tuz <alex-tuz@yandex.ru>
2025-08-01 07:52:43 -0500

    refactor: replace hardcoded values with CSS variables cy-21

--- apps/frontend/src/pages/home/components/feedbacks-section/feedbacks-section.tsx
+++ apps/frontend/src/pages/home/components/feedbacks-section/feedbacks-section.tsx
@@ -14,7 +14,6 @@ const FeedbacksSection: FC = () => {
 				"grid-pattern",
 				styles["landing-section"],
 				styles["light-background"],
-				// styles["feedbacks-section"],
 			)}
 		>
 			<div className="wrapper">
