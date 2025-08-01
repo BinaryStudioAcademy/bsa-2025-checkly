54e12fee0226b14a7753c5098bc0aee345e4ceed
Amicuchi <AndersonAmicuchi@gmail.com>
2025-08-01 08:07:15 -0300

    fix: excluding unused logo export cy-22

--- apps/frontend/src/assets/img/footer/index.ts
+++ apps/frontend/src/assets/img/footer/index.ts
@@ -1,4 +1,3 @@
-export { default as Logo } from "../shared/logo-icon.svg?react";
 export { default as FbIcon } from "./fb.svg?react";
 export { default as IgIcon } from "./ig.svg?react";
 export { default as YtIcon } from "./yt.svg?react";
