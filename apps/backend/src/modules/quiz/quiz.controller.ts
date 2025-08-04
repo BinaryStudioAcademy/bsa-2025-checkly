3c785d04beb1c8ed572baa0dcd7dbe8a845202f8
watchlar2000 <sergii.koch@gmail.com>
2025-08-04 13:49:26 +0200

    feat: + add quiz controller cy-54

--- apps/backend/src/modules/quiz/quiz.controller.ts
+++ apps/backend/src/modules/quiz/quiz.controller.ts
@@ -1,26 +1,35 @@
-import { APIPath, HTTPCode } from "shared";
+import { APIPath, HTTPCode, QuizzApiPath } from "shared";
 import { HTTPMethod } from "shared/src/libs/modules/http/libs/enums/http-method.enum.js";
 
-import { BaseController } from "~/libs/modules/controller/controller.js";
+import {
+	type APIHandlerResponse,
+	BaseController,
+} from "~/libs/modules/controller/controller.js";
 import { type Logger } from "~/libs/modules/logger/logger.js";
 
+import { type QuizService } from "./quiz.service.js";
+
 class QuizContoller extends BaseController {
-	public constructor(logger: Logger) {
+	private quizService: QuizService;
+
+	public constructor(logger: Logger, quizService: QuizService) {
 		super(logger, APIPath.QUIZ);
 
+		this.quizService = quizService;
+
 		this.addRoute({
-			handler: () => {
-				return {
-					payload: {
-						message: "Hello World",
-					},
-					status: HTTPCode.OK,
-				};
-			},
+			handler: () => this.getAllQuestionsWithOptions(),
 			isPublic: true,
 			method: HTTPMethod.GET,
-			path: "/",
+			path: QuizzApiPath.QUESTIONS,
 		});
+	}
+
+	private async getAllQuestionsWithOptions(): Promise<APIHandlerResponse> {
+		return {
+			payload: await this.quizService.findAllQuestionOptions(),
+			status: HTTPCode.OK,
+		};
 	}
 }
 
