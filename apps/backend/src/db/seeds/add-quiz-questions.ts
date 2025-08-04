adb0b20dc5f08298b28767bbc7f6f05343bc4448
watchlar2000 <sergii.koch@gmail.com>
2025-08-04 22:50:01 +0200

    feat: + add questions seed cy-54

---
+++ apps/backend/src/db/seeds/add-quiz-questions.ts
@@ -0,0 +1,45 @@
+import { type Knex } from "knex";
+import { QUESTION_TYPE } from "shared";
+
+import { DatabaseTableName } from "~/libs/modules/database/database.js";
+
+async function seed(knex: Knex): Promise<void> {
+	await knex(DatabaseTableName.QUESTION_OPTIONS).del();
+	await knex(DatabaseTableName.QUESTIONS).del();
+
+	await knex(DatabaseTableName.QUESTIONS).insert([
+		{
+			id: 1,
+			is_optional: false,
+			order: 1,
+			text: "What's your current goal or focus area?",
+			type: QUESTION_TYPE.MULTIPLE_CHOICE_WITH_TEXT_INPUT,
+		},
+		{
+			id: 2,
+			is_optional: false,
+			order: 2,
+			text: "How much time can you realistically dedicate per day?",
+			type: QUESTION_TYPE.SINGLE_CHOICE,
+		},
+	]);
+
+	await knex("question_options").insert([
+		{ order: 1, question_id: 1, text: "💪 Health & Fitness" },
+		{ order: 2, question_id: 1, text: "🧠 Personal Growth" },
+		{ order: 3, question_id: 1, text: "💸 Money & Finances" },
+		{ order: 4, question_id: 1, text: "💼 Career & Productivity" },
+		{ order: 5, question_id: 1, text: "🧘 Mindfulness & Mental Health" },
+		{ order: 6, question_id: 1, text: "🎨 Creativity & Art" },
+		{ order: 7, question_id: 1, text: "🧩 Hobbies & Trying New Things" },
+		{ order: 8, question_id: 1, text: "✍️ Other" },
+
+		{ order: 1, question_id: 2, text: "⏱ 10–15 min" },
+		{ order: 2, question_id: 2, text: "⏱ 20–30 min" },
+		{ order: 3, question_id: 2, text: "⏱ 45–60 min" },
+		{ order: 4, question_id: 2, text: "⏱ 1–2 hours" },
+		{ order: 5, question_id: 2, text: "⏱ More than 2 hours" },
+	]);
+}
+
+export { seed };
