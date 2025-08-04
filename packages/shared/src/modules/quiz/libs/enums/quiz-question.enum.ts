9e86ff69bccfcfd89f3a70d1ae85475821d5f26f
watchlar2000 <sergii.koch@gmail.com>
2025-08-04 13:47:13 +0200

    feat: + add quiz enums to shared folder cy-54

---
+++ packages/shared/src/modules/quiz/libs/enums/quiz-question.enum.ts
@@ -0,0 +1,9 @@
+const QUESTION_TYPE = {
+	MULTIPLE_CHOICE: "multiple_choice",
+	MULTIPLE_CHOICE_WITH_TEXT_INPUT: "multiple_choice_with_text_input",
+	SINGLE_CHOICE: "single_choice",
+	SINGLE_CHOICE_WITH_TEXT_INPUT: "single_choice_with_text_input",
+	TEXT_INPUT: "text_input",
+} as const;
+
+export { QUESTION_TYPE };
