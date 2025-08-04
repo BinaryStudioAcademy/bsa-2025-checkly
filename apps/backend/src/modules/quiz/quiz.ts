c3888f65fa870397d37e9a8cd2ae7a867299aac2
watchlar2000 <sergii.koch@gmail.com>
2025-08-04 13:49:59 +0200

    feat: + init quiz cy-54

--- apps/backend/src/modules/quiz/quiz.ts
+++ apps/backend/src/modules/quiz/quiz.ts
@@ -1,7 +1,13 @@
 import { logger } from "~/libs/modules/logger/logger.js";
 
+import { QuestionOptionsModel } from "./libs/models/question-options.model.js";
+import { QuestionModel } from "./libs/models/questions.model.js";
 import { QuizContoller } from "./quiz.controller.js";
+import { QuizRepository } from "./quiz.repository.js";
+import { QuizService } from "./quiz.service.js";
 
-const quizContolller = new QuizContoller(logger);
+const quizRepository = new QuizRepository(QuestionModel, QuestionOptionsModel);
+const quizService = new QuizService(quizRepository);
+const quizContolller = new QuizContoller(logger, quizService);
 
 export { quizContolller };
