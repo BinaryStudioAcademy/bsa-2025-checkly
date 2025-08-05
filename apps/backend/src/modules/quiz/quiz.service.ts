52b65f73d2887216edc4b4eba02e9592ac5292d2
watchlar2000 <sergii.koch@gmail.com>
2025-08-05 22:57:13 +0200

    feat: * update quiz service cy-54

--- apps/backend/src/modules/quiz/quiz.service.ts
+++ apps/backend/src/modules/quiz/quiz.service.ts
@@ -1,4 +1,8 @@
-import { type QuizQuestionsResponseDto } from "shared/src/modules/quiz/quiz.js";
+import {
+	type QuizAnswersRequestDto,
+	type QuizAnswersResponseDto,
+	type QuizQuestionsResponseDto,
+} from "shared";
 
 import { type QuizRepository } from "./quiz.repository.js";
 
@@ -14,6 +18,19 @@ class QuizService {
 
 		return {
 			items: data.map((question) => question.toObject()),
+		};
+	}
+
+	public async submitAnswers(
+		body: QuizAnswersRequestDto,
+	): Promise<QuizAnswersResponseDto> {
+		const ANSWERS_PROCESSING_TIME = 0;
+		await new Promise((resolve) =>
+			setTimeout(resolve, ANSWERS_PROCESSING_TIME),
+		);
+
+		return {
+			response: body,
 		};
 	}
 }
