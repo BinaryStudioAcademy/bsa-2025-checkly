76a9ee4397f745fc337b7541939bd3f493d72f36
watchlar2000 <sergii.koch@gmail.com>
2025-08-05 13:54:56 +0200

    feat: * update quiz models cy-54

--- apps/backend/src/modules/quiz/libs/models/models.ts
+++ apps/backend/src/modules/quiz/libs/models/models.ts
@@ -1,2 +1,2 @@
-export { QuestionOptionsModel } from "./question-options.model.js";
-export { QuestionModel } from "./questions.model.js";
+export { QuestionOptionModel } from "./question-option.model.js";
+export { QuestionModel } from "./question.model.js";
