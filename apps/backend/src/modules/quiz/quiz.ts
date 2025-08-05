23fa1d322ca48362be4f1a870c590a9b2cfabeeb
watchlar2000 <sergii.koch@gmail.com>
2025-08-05 22:58:18 +0200

    feat: * update quiz module index cy-54

--- apps/backend/src/modules/quiz/quiz.ts
+++ apps/backend/src/modules/quiz/quiz.ts
@@ -1,12 +1,12 @@
 import { logger } from "~/libs/modules/logger/logger.js";
 
 import { QuestionModel } from "./libs/models/question.model.js";
-import { QuizContoller } from "./quiz.controller.js";
+import { QuizController } from "./quiz.controller.js";
 import { QuizRepository } from "./quiz.repository.js";
 import { QuizService } from "./quiz.service.js";
 
 const quizRepository = new QuizRepository(QuestionModel);
 const quizService = new QuizService(quizRepository);
-const quizContolller = new QuizContoller(logger, quizService);
+const quizContolller = new QuizController(logger, quizService);
 
 export { quizContolller };
