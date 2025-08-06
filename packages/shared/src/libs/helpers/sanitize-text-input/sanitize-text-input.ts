f5eda723241b06d395e08d17ec85aaa8f204ce2f
watchlar2000 <sergii.koch@gmail.com>
2025-08-06 14:04:37 +0200

    feat: * update sanitize-text-input util cy-54

--- packages/shared/src/libs/helpers/sanitize-text-input/sanitize-text-input.ts
+++ packages/shared/src/libs/helpers/sanitize-text-input/sanitize-text-input.ts
@@ -1,10 +1,11 @@
 const MIN_INPUT_LENGTH = 0;
-const MAX_INPUT_LENGTH = 1000;
+const MAX_INPUT_LENGTH = 200;
 
 const sanitizeTextInput = (input: string): string =>
 	input
 		.trim()
-		.replaceAll(/[<>'"&]/g, "")
+		.replaceAll(/[<>'"&`$]/g, "")
+		.replaceAll(/[\n\r\t]/g, "")
 		.replaceAll(/\s+/g, " ")
 		.slice(MIN_INPUT_LENGTH, MAX_INPUT_LENGTH);
 
