40060fed94eb0a742d8a147411637d667921e6d6
muxkuro <max.tolstopyat@gmail.com>
2025-08-07 00:02:15 +0200

    fix: fixed email input type cy-172

--- apps/frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
+++ apps/frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
@@ -103,7 +103,7 @@ const SignInForm: React.FC<Properties> = ({ onSubmit }: Properties) => {
 							name="email"
 							placeholder={AUTH_PLACEHOLDERS.email}
 							required
-							type="email"
+							type="text"
 						/>
 						<Input
 							control={control}
