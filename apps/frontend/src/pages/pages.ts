89eaf916e565fe8ef640d9ca14c81e3e67d6ebfd
muxkuro <max.tolstopyat@gmail.com>
2025-08-06 18:34:37 +0200

    fix: fixing linter errors cy-15

--- apps/frontend/src/pages/pages.ts
+++ apps/frontend/src/pages/pages.ts
@@ -2,3 +2,4 @@ export { Auth } from "./auth/auth.js";
 export { Home } from "./home/home.js";
 export { QuestionFlow } from "./quiz/questions/question-flow.js";
 export { Quiz } from "./quiz/quiz.js";
+
