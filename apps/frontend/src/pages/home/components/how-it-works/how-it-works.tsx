a00e17ac8cf1633a526b87a7d3e9137b63acbb54
watchlar2000 <sergii.koch@gmail.com>
2025-08-07 14:31:02 +0200

    fix: * update how-it-works section cy-179

--- apps/frontend/src/pages/home/components/how-it-works/how-it-works.tsx
+++ apps/frontend/src/pages/home/components/how-it-works/how-it-works.tsx
@@ -16,49 +16,45 @@ const HowItWorks: React.FC = () => {
 			className={getClassNames("grid-pattern", styles["section"])}
 			data-section-variant="brand"
 		>
-			<div className={styles["wrapper"]}>
-				<div className={styles["container"]}>
-					<h2 className={styles["title"]}>How it works</h2>
-					<div className={styles["stepsContainer"]}>
-						<div className={stepBlockClasses}>
-							<div className={styles["stepNumber"]}>1</div>
-							<p className={styles["stepTitle"]}>Take the quiz</p>
-						</div>
-						<div className={styles["arrow"]}>
-							<DecorativeImage
-								className={getClassNames(styles["arrowImage"])}
-								src={Arrow}
-							/>
-						</div>
-						<div className={stepBlockClasses}>
-							<div className={styles["stepNumber"]}>2</div>
-							<p className={styles["stepTitle"]}>Get your plan</p>
-						</div>
-						<div className={styles["arrow"]}>
-							<DecorativeImage
-								className={getClassNames(styles["arrowImage"])}
-								src={Arrow}
-							/>
-						</div>
-						<div className={stepBlockClasses}>
-							<div className={styles["stepNumber"]}>3</div>
-							<p className={styles["stepTitle"]}>
-								Download PDF or customize it
-							</p>
-						</div>
+			<div className={getClassNames("wrapper", styles["container"])}>
+				<h2 className={styles["title"]}>How it works</h2>
+				<div className={styles["stepsContainer"]}>
+					<div className={stepBlockClasses}>
+						<div className={styles["stepNumber"]}>1</div>
+						<p className={styles["stepTitle"]}>Take the quiz</p>
 					</div>
-					<div className={styles["croissantImage"]}>
-						<DecorativeImage src={Croissant} />
-					</div>
-					<div className={styles["laptopImage"]}>
-						<DecorativeImage src={Laptop} />
-					</div>
-					<div className={styles["decorativeStars"]}>
+					<div className={styles["arrow"]}>
 						<DecorativeImage
-							className={getClassNames(styles["starsImage"])}
-							src={StarsYellow02}
+							className={getClassNames(styles["arrowImage"])}
+							src={Arrow}
 						/>
 					</div>
+					<div className={stepBlockClasses}>
+						<div className={styles["stepNumber"]}>2</div>
+						<p className={styles["stepTitle"]}>Get your plan</p>
+					</div>
+					<div className={styles["arrow"]}>
+						<DecorativeImage
+							className={getClassNames(styles["arrowImage"])}
+							src={Arrow}
+						/>
+					</div>
+					<div className={stepBlockClasses}>
+						<div className={styles["stepNumber"]}>3</div>
+						<p className={styles["stepTitle"]}>Download PDF or customize it</p>
+					</div>
+				</div>
+				<div className={styles["croissantImage"]}>
+					<DecorativeImage src={Croissant} />
+				</div>
+				<div className={styles["laptopImage"]}>
+					<DecorativeImage src={Laptop} />
+				</div>
+				<div className={styles["decorativeStars"]}>
+					<DecorativeImage
+						className={getClassNames(styles["starsImage"])}
+						src={StarsYellow02}
+					/>
 				</div>
 			</div>
 		</section>
