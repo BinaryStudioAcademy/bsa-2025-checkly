47d90a9d6bf2c0dd352dbd52b66ab79257d66b9b
alex-tuz <alex-tuz@yandex.ru>
2025-08-08 10:39:26 -0500

    feat: implement basic dashboard component styles cy-44

--- apps/frontend/src/libs/components/dashboard/dashboard.tsx
+++ apps/frontend/src/libs/components/dashboard/dashboard.tsx
@@ -1,9 +1,17 @@
 import { type FC } from "react";
 
+import { getClassNames } from "~/libs/helpers/get-class-names.js";
+
+import styles from "./styles.module.css";
+
+const classMainContent = getClassNames(
+	"grid-pattern",
+	styles["light-background"],
+	styles["mainContent"],
+);
+
 const Dashboard: FC = () => {
-	return (
-		<div>Dashboard</div>
-	);
+	return <main className={classMainContent} />;
 };
 
 export { Dashboard };
