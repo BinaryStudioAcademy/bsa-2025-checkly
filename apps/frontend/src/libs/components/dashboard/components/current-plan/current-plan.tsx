1c69c85e7cc60a870d6842e67831e03c052f7341
alex-tuz <alex-tuz@yandex.ru>
2025-08-08 13:18:07 -0500

    feat: add navigation to plan page in current plan component cy-89

--- apps/frontend/src/libs/components/dashboard/components/current-plan/current-plan.tsx
+++ apps/frontend/src/libs/components/dashboard/components/current-plan/current-plan.tsx
@@ -1,11 +1,21 @@
-import { type FC } from "react";
+import { type FC, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { PlanBig } from "~/assets/img/shared/illustrations/layouts/layouts.img.js";
+import { AppRoute } from "~/libs/enums/app-route.enum.js";
 
 import { Button } from "../button/button.js";
 import styles from "./styles.module.css";
 
 const CurrentPlan: FC = () => {
+	const navigate = useNavigate();
+
+	const handleClick = useCallback((): void => {
+		void (async (): Promise<void> => {
+			await navigate(AppRoute.PLAN);
+		})();
+	}, [navigate]);
+
 	return (
 		<div className={styles["container"]}>
 			<h2 className={styles["title"]}>Current active plan</h2>
@@ -16,7 +26,11 @@ const CurrentPlan: FC = () => {
 					src={PlanBig}
 				/>
 			</div>
-			<Button className={styles["actionButton"]} label="CONTINUE" />
+			<Button
+				className={styles["actionButton"]}
+				label="CONTINUE"
+				onClick={handleClick}
+			/>
 		</div>
 	);
 };
