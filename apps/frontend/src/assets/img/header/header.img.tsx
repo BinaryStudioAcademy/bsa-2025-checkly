6283f4e80b7fe579914e7f5344d74c53e265562b
Amicuchi <AndersonAmicuchi@gmail.com>
2025-08-08 21:55:12 -0300

    feat: header arrow cy-68

--- apps/frontend/src/assets/img/header/header.img.tsx
+++ apps/frontend/src/assets/img/header/header.img.tsx
@@ -1,2 +1,4 @@
 export { default as arrowDown } from "./arrow-down.svg";
+export { default as arrowLeft } from "./arrow-left.svg";
+export { default as logo } from "./logo.svg";
 export { default as profileDefault } from "./profile-default.png";
