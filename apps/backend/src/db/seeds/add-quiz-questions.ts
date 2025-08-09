b652733a54c38d530b95e7e6a2931ea4b65c758d
watchlar2000 <sergii.koch@gmail.com>
2025-08-09 23:32:29 +0200

    feat: * update seeds cy-54

--- apps/backend/src/db/seeds/add-quiz-questions.ts
+++ apps/backend/src/db/seeds/add-quiz-questions.ts
@@ -1,7 +1,14 @@
 import { type Knex } from "knex";
-import { QuizQuestionFormat } from "shared";
 
 import { DatabaseTableName } from "~/libs/modules/database/database.js";
+
+const QuizQuestionFormat = {
+	MULTIPLE_CHOICE: "multiple_choice",
+	MULTIPLE_CHOICE_WITH_TEXT_INPUT: "multiple_choice_with_text_input",
+	SINGLE_CHOICE: "single_choice",
+	SINGLE_CHOICE_WITH_TEXT_INPUT: "single_choice_with_text_input",
+	TEXT_INPUT: "text_input",
+} as const;
 
 async function seed(knex: Knex): Promise<void> {
 	await knex(DatabaseTableName.QUESTION_OPTIONS).del();
