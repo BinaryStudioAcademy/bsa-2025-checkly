9f0a13dba7f10ab59cb9fa80ddbe1883c1971872
Amicuchi <AndersonAmicuchi@gmail.com>
2025-08-11 19:43:15 -0300

    feat: add plan preview cy-68

--- apps/frontend/src/libs/components/components.ts
+++ apps/frontend/src/libs/components/components.ts
@@ -6,6 +6,7 @@ export { DecorativeImage } from "./decorative-image/decorative-image.js";
 export { Input } from "./input/input.js";
 export { Link } from "./link/link.js";
 export { Loader } from "./loader/loader.js";
+export { PlanPreview } from "./plan-preview/plan-preview.js";
 export { ProtectedRoute } from "./protected-route/protected-route.js";
 export { RouterProvider } from "./router-provider/router-provider.js";
 export { Textarea } from "./textarea/textarea.js";
