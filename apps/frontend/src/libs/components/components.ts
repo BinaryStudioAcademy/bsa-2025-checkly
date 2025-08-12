040dab85dc40f6a75646dbdcb0336b27d7c992f0
Amicuchi <AndersonAmicuchi@gmail.com>
2025-08-11 21:32:07 -0300

    style: fixing components exports cy-68

--- apps/frontend/src/libs/components/components.ts
+++ apps/frontend/src/libs/components/components.ts
@@ -1,15 +1,15 @@
 export { AppHeader } from "./app-header/app-header.js";
 export { App } from "./app/app.js";
 export { Button } from "./button/button.js";
-export { DaysNav } from "./days-nav/days-nav.js";
 export { DashboardWrapper } from "./dashboard-wrapper/dashboard-wrapper.js";
 export { Dashboard } from "./dashboard/dashboard.js";
+export { DaysNav } from "./days-nav/days-nav.js";
 export { DecorativeImage } from "./decorative-image/decorative-image.js";
 export { Input } from "./input/input.js";
 export { Link } from "./link/link.js";
 export { Loader } from "./loader/loader.js";
-export { PlanPreview } from "./plan-preview/plan-preview.js";
 export { Logo } from "./logo/logo.js";
+export { PlanPreview } from "./plan-preview/plan-preview.js";
 export { ProtectedRoute } from "./protected-route/protected-route.js";
 export { RouterProvider } from "./router-provider/router-provider.js";
 export { Textarea } from "./textarea/textarea.js";
