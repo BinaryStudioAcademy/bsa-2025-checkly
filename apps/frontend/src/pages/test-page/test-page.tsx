2367fb92d7ae19ba1c39f72276469de7fb5e9656
Kaluha Bohdan <bkdragon232@gmail.com>
2025-08-13 16:41:34 +0300

    fix: add styles file for test-page cy-160

--- apps/frontend/src/pages/test-page/test-page.tsx
+++ apps/frontend/src/pages/test-page/test-page.tsx
@@ -2,37 +2,20 @@ import { AppHeader } from "~/libs/components/components.js";
 import { PlanStyle } from "~/libs/components/plan-styles/plan-style/plan-style.js";
 import { type PlanStyleOption } from "~/libs/types/types.js";
 
+import styles from "./styles.module.css";
+
 const SELECTED_STYLE: PlanStyleOption = "withremarks";
 
 const TestPage: React.FC = () => {
 	return (
 		<>
 			<AppHeader />
-			<main
-				style={{
-					display: "flex",
-					flexDirection: "column",
-					height: "100vh",
-					paddingTop: 64,
-				}}
-			>
-				<div
-					style={{
-						backgroundColor: "#f1f1f1",
-						borderBottom: "1px solid #000",
-						height: 64,
-					}}
-				/>
-				<div style={{ flex: "1 1 auto", overflowY: "auto" }}>
+			<main className={styles["test-main"]}>
+				<div className={styles["test-panel"]} />
+				<div className={styles["test-body"]}>
 					<PlanStyle inputStyle={SELECTED_STYLE} />
 				</div>
-				<div
-					style={{
-						backgroundColor: "#f1f1f1",
-						borderBottom: "1px solid #000",
-						height: 64,
-					}}
-				/>
+				<div className={styles["test-panel"]} />
 			</main>
 		</>
 	);
