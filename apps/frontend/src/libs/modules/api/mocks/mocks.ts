0b990db0ad03dc7031ae64441762645d0d8fe182
Kaluha Bohdan <bkdragon232@gmail.com>
2025-08-15 17:31:33 +0300

    fix: adjust dashboard styles cy-160

--- apps/frontend/src/libs/modules/api/mocks/mocks.ts
+++ apps/frontend/src/libs/modules/api/mocks/mocks.ts
@@ -2,7 +2,7 @@ import { AppEnvironment } from "~/libs/enums/enums.js";
 import { config } from "~/libs/modules/config/config.js";
 
 async function enableMocking(): Promise<ServiceWorkerRegistration | undefined> {
-	if (config.ENV.APP.ENVIRONMENT !== AppEnvironment.LOCAL) {
+	if (config.ENV.APP.ENVIRONMENT === AppEnvironment.LOCAL) {
 		return;
 	}
 
