7fc337f662577e36d4f1b459dcb9cc151dc08083
watchlar2000 <sergii.koch@gmail.com>
2025-08-16 11:26:35 +0200

    feat: * update current-plan markup and styles cy-171

--- apps/frontend/src/libs/components/dashboard/components/current-plan/current-plan.tsx
+++ apps/frontend/src/libs/components/dashboard/components/current-plan/current-plan.tsx
@@ -3,21 +3,25 @@ import { type FC } from "react";
 import { Link } from "~/libs/components/components.js";
 import { PlanStyle } from "~/libs/components/plan-styles/plan-style/plan-style.js";
 import { AppRoute } from "~/libs/enums/app-route.enum.js";
+import { getClassNames } from "~/libs/helpers/get-class-names.js";
 
 import styles from "./styles.module.css";
 
 const CurrentPlan: FC = () => {
 	return (
-		<div className={styles["container"]}>
+		<div className={getClassNames("cluster", styles["container"])}>
 			<h2 className={styles["title"]}>Current active plan</h2>
 			<div className={styles["plan-card"]}>
 				<PlanStyle inputStyle="withremarks" />
 			</div>
-			<div className={styles["continue-button"]}>
-				<Link asButtonSize="small" asButtonVariant="primary" to={AppRoute.PLAN}>
-					Continue
-				</Link>
-			</div>
+			<Link
+				asButtonSize="small"
+				asButtonVariant="primary"
+				className={getClassNames(styles["continue-button"])}
+				to={AppRoute.PLAN}
+			>
+				Continue
+			</Link>
 		</div>
 	);
 };
