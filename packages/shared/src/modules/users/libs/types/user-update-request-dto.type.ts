ef42424443598c3ee36b1ed99fed57a8c3ea50ba
alex-tuz <alex-tuz@yandex.ru>
2025-08-16 20:54:57 -0500

    refactor: apply requested changes from code review cy-176

--- packages/shared/src/modules/users/libs/types/user-update-request-dto.type.ts
+++ packages/shared/src/modules/users/libs/types/user-update-request-dto.type.ts
@@ -1,9 +1,7 @@
-type UserUpdateRequestDto = {
-	confirmPassword?: string | undefined;
-	dob?: string | undefined;
-	email?: string | undefined;
-	name?: string | undefined;
-	password?: string | undefined;
-};
+import { type z } from "zod";
+
+import { type userUpdate } from "../validation-schemas/user-update.validation-schema.js";
+
+type UserUpdateRequestDto = z.infer<typeof userUpdate>;
 
 export { type UserUpdateRequestDto };
