588f705b23a0929033d98514bc06453a31395c93
astowner <the1samevl@gmail.com>
2025-08-18 10:14:14 +0300

    feat: update migration, add created_at, updated_at columns cy-202

--- apps/backend/src/db/migrations/20250815124137_add_question_answers_table.ts
+++ apps/backend/src/db/migrations/20250815124137_add_question_answers_table.ts
@@ -10,21 +10,27 @@ const TABLE_NAMES = {
 };
 
 const QuizColumnName = {
+	CREATED_AT: "created_at",
 	ID: "id",
+	UPDATED_AT: "updated_at",
 } as const;
 
 const AnswerColumnName = {
+	CREATED_AT: "created_at",
 	ID: "id",
 	IS_SKIPPED: "is_skipped",
 	QUESTION_ID: "question_id",
 	QUIZ_ID: "quiz_id",
+	UPDATED_AT: "updated_at",
 	USER_INPUT: "user_input",
 } as const;
 
 const AnswerOptionColumnName = {
 	ANSWER_ID: "answer_id",
+	CREATED_AT: "created_at",
 	ID: "id",
 	OPTION_ID: "option_id",
+	UPDATED_AT: "updated_at",
 } as const;
 
 const QuestionColumnName = {
@@ -39,6 +45,10 @@ const PlanColumnName = {
 	QUIZ_ID: "quiz_id",
 } as const;
 
+const COLUMN_LENGTH = {
+	USER_INPUT: 150,
+};
+
 async function down(knex: Knex): Promise<void> {
 	await knex.schema.alterTable(TABLE_NAMES.PLANS, (table) => {
 		table.dropColumn(PlanColumnName.QUIZ_ID);
@@ -52,6 +62,14 @@ async function down(knex: Knex): Promise<void> {
 async function up(knex: Knex): Promise<void> {
 	await knex.schema.createTable(TABLE_NAMES.QUIZ, (table) => {
 		table.increments(QuizColumnName.ID).primary();
+		table
+			.timestamp(QuizColumnName.CREATED_AT, { useTz: true })
+			.notNullable()
+			.defaultTo(knex.fn.now());
+		table
+			.dateTime(QuizColumnName.UPDATED_AT)
+			.notNullable()
+			.defaultTo(knex.fn.now());
 	});
 
 	await knex.schema.createTable(TABLE_NAMES.ANSWERS, (table) => {
@@ -68,8 +86,18 @@ async function up(knex: Knex): Promise<void> {
 			.references(QuestionColumnName.ID)
 			.inTable(TABLE_NAMES.QUESTIONS)
 			.onDelete("CASCADE");
-		table.text(AnswerColumnName.USER_INPUT);
+		table
+			.string(AnswerColumnName.USER_INPUT, COLUMN_LENGTH.USER_INPUT)
+			.notNullable();
 		table.boolean(AnswerColumnName.IS_SKIPPED).notNullable().defaultTo(false);
+		table
+			.timestamp(AnswerColumnName.CREATED_AT, { useTz: true })
+			.notNullable()
+			.defaultTo(knex.fn.now());
+		table
+			.dateTime(AnswerColumnName.UPDATED_AT)
+			.notNullable()
+			.defaultTo(knex.fn.now());
 	});
 
 	await knex.schema.createTable(TABLE_NAMES.ANSWER_OPTIONS, (table) => {
@@ -86,6 +114,14 @@ async function up(knex: Knex): Promise<void> {
 			.references(OptionColumnName.ID)
 			.inTable(TABLE_NAMES.QUESTION_OPTIONS)
 			.onDelete("CASCADE");
+		table
+			.timestamp(AnswerOptionColumnName.CREATED_AT, { useTz: true })
+			.notNullable()
+			.defaultTo(knex.fn.now());
+		table
+			.dateTime(AnswerOptionColumnName.UPDATED_AT)
+			.notNullable()
+			.defaultTo(knex.fn.now());
 	});
 
 	await knex.schema.alterTable(TABLE_NAMES.PLANS, (table) => {
