3ca84b636d7b2238fb14814c6619a634fc7013b1
watchlar2000 <sergii.koch@gmail.com>
2025-08-19 11:45:12 +0200

    feat: + add zero-category-order const cy-171

--- apps/frontend/src/libs/components/dashboard/components/past-plans/libs/enums/default-category-option.enum.ts
+++ apps/frontend/src/libs/components/dashboard/components/past-plans/libs/enums/default-category-option.enum.ts
@@ -1,9 +1,11 @@
 import { ZERO_CATEGORY_ID } from "../../../libs/enums/enums.js";
 
+const ZERO_CATEGORY_ORDER = 0;
+
 const defaultCategoryOption = {
 	iconHref: undefined,
 	id: ZERO_CATEGORY_ID,
-	order: 0,
+	order: ZERO_CATEGORY_ORDER,
 	title: "All categories",
 } as const;
 
