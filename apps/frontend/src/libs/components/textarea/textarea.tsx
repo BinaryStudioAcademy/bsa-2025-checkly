2ae019d64eb965fa8901ac7d6d868c530388b854
Amicuchi <AndersonAmicuchi@gmail.com>
2025-08-19 13:37:45 -0300

    feat: creating the textArea component cy-68

---
+++ apps/frontend/src/libs/components/textarea/textarea.tsx
@@ -0,0 +1,89 @@
+import { type JSX, useId } from "react";
+import {
+	type Control,
+	type FieldErrors,
+	type FieldPath,
+	type FieldValues,
+} from "react-hook-form";
+
+import { getClassNames } from "~/libs/helpers/get-class-names.js";
+import { useFormController } from "~/libs/hooks/hooks.js";
+
+import styles from "./styles.module.css";
+
+const ROWS = 4;
+const MIN_CHARACTERS = 0;
+
+type Properties<T extends FieldValues> = {
+	control: Control<T, null>;
+	errors: FieldErrors<T>;
+	label: string;
+	maxLength?: number;
+	name: FieldPath<T>;
+	placeholder?: string;
+	required?: boolean;
+	resize?: "both" | "horizontal" | "none" | "vertical";
+	rows?: number;
+};
+
+const Textarea = <T extends FieldValues>({
+	control,
+	errors,
+	label,
+	maxLength,
+	name,
+	placeholder = "",
+	required,
+	resize = "vertical",
+	rows = ROWS,
+}: Properties<T>): JSX.Element => {
+	const textareaId = useId();
+	const { field } = useFormController({ control, name });
+	const error = errors[name]?.message;
+	const hasError = Boolean(error);
+
+	const textareaContainerClass = getClassNames(
+		"cluster",
+		styles["textarea-container"],
+	);
+	const textareaFieldClass = getClassNames(
+		styles["textarea-field"],
+		hasError && styles["textarea-field--error"],
+		styles[`textarea-field--resize-${resize}`],
+	);
+
+	const characterCount = (field.value as string).length || MIN_CHARACTERS;
+	const showCharacterCount = Boolean(maxLength);
+
+	return (
+		<div className={textareaContainerClass}>
+			<label className={styles["textarea-label"]} htmlFor={textareaId}>
+				{label}
+			</label>
+			<div className={styles["textarea-wrapper"]}>
+				<textarea
+					{...field}
+					aria-invalid={hasError}
+					className={textareaFieldClass}
+					id={textareaId}
+					maxLength={maxLength}
+					name={name}
+					placeholder={placeholder}
+					required={required}
+					rows={rows}
+				/>
+				{showCharacterCount && (
+					<div className={styles["character-count"]}>
+						{characterCount}
+						{maxLength && `/${String(maxLength)}`}
+					</div>
+				)}
+			</div>
+			{hasError && (
+				<p className={styles["textarea-field__error"]}>{error as string}</p>
+			)}
+		</div>
+	);
+};
+
+export { Textarea };
