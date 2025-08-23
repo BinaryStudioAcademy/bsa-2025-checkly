79f3f203ad837f92864d3cd7270e11fd43db05e3
Volin13 <olegkukharishin@gmail.com>
2025-08-23 21:20:24 +0300

    feat(frontend): added loop and autoplay fixed issues cy-170

--- apps/frontend/src/pages/home/components/feedbacks-section/feedback-loader-container/feedback-loader-container.tsx
+++ apps/frontend/src/pages/home/components/feedbacks-section/feedback-loader-container/feedback-loader-container.tsx
@@ -1,10 +1,11 @@
 import { Loader } from "~/libs/components/components.js";
+import { getClassNames } from "~/libs/helpers/get-class-names.js";
 
 import styles from "../styles.module.css";
 
 const FeedbackLoaderContainer: React.FC = () => {
 	return (
-		<div className={styles["loader-container"]}>
+		<div className={getClassNames("cluster", styles["loader-container"])}>
 			<Loader container="inline" size="large" />
 		</div>
 	);
