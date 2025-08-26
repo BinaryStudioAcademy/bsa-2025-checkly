c4a382be8fe300426aed1f53f552e3fa45056a2c
olenkabilonizhka <olenka.bilonizhka@gmail.com>
2025-08-26 09:27:42 +0300

    feat: add daytime icons to pdf cy-324

--- apps/frontend/src/libs/components/plan-styles/components/task/task.tsx
+++ apps/frontend/src/libs/components/plan-styles/components/task/task.tsx
@@ -1,20 +1,34 @@
+import React, { type JSX } from "react";
+import { IoMoonOutline as EveningIcon } from "react-icons/io5";
+import { LuSun as AfternoonIcon } from "react-icons/lu";
+import { TbSunset2 as MorningIcon } from "react-icons/tb";
+
 import { PlanStyleModules } from "~/libs/enums/plan-style-modules.enum.js";
 import { getClassNames } from "~/libs/helpers/helpers.js";
 import { type PlanStyleOption } from "~/libs/types/types.js";
 
+import { ExecutionTimeType } from "../../libs/types/types.js";
 import styles from "./styles.module.css";
 
 type Properties = {
+	executionTimeType?: string;
 	id: string;
 	inputStyle: PlanStyleOption;
 	taskText: string;
 };
 
 const Task: React.FC<Properties> = ({
+	executionTimeType,
 	id,
 	inputStyle,
 	taskText,
 }: Properties) => {
+	const icons: Record<string, JSX.Element> = {
+		[ExecutionTimeType.AFTERNOON]: <AfternoonIcon className={styles["icon"]} />,
+		[ExecutionTimeType.EVENING]: <EveningIcon className={styles["icon"]} />,
+		[ExecutionTimeType.MORNING]: <MorningIcon className={styles["icon"]} />,
+	};
+
 	const taskClasses = getClassNames(
 		styles["task"],
 		PlanStyleModules[inputStyle]["task"],
@@ -42,6 +56,7 @@ const Task: React.FC<Properties> = ({
 			<label className={taskTextClasses} htmlFor={id}>
 				{taskText}
 			</label>
+			{executionTimeType ? icons[executionTimeType] : null}
 		</li>
 	);
 };
