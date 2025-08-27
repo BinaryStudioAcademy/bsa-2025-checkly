d5619cec497b015c4031fa14603801b7e5e132f9
Nazar Kovtun <nazar.kovtun@hackolade.com>
2025-08-27 10:48:27 +0300

    fix: add chromium dep for plan export to pdf cy-0

---
+++ .hooks/prebuild/01_install_chromium_deps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo yum install -y \
+  atk \
+  atk-devel \
+  gtk3 \
+  nss \
+  alsa-lib \
+  libXcomposite \
+  libXcursor \
+  libXdamage \
+  libXext \
+  libXi \
+  libXtst \
+  libXrandr \
+  cups-libs \
+  pango \
+  xorg-x11-server-Xvfb \
+  libXss \
+  libXScrnSaver
