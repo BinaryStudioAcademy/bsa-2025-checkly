10caea2c6efb521b27cd8f1dcf9377d80c6c33dc
watchlar2000 <sergii.koch@gmail.com>
2025-08-28 11:09:09 +0200

    feat: * update icons cy-300

--- apps/frontend/src/assets/img/icons/icons.tsx
+++ apps/frontend/src/assets/img/icons/icons.tsx
@@ -1,4 +1,4 @@
-export { default as ArrowLeft } from "./arrow-left.svg";
+export { default as ArrowLeft } from "./arrow-left.svg?react";
 export { default as ArrowLeftIcon } from "./arrow-left.svg?react";
 export { default as CreateDoc } from "./create_doc.svg?react";
 export { default as Dashboard } from "./dashboard.svg?react";
@@ -13,7 +13,7 @@ export { default as MonitorIcon } from "./monitor.svg?react";
 export { default as Palette } from "./palette.svg?react";
 export { default as Regenerate } from "./regenerate.svg";
 export { default as Remove } from "./remove.svg";
-export { default as Save } from "./save.svg";
+export { default as Save } from "./save.svg?react";
 export { default as Search } from "./search.svg?react";
 export { default as SmartphoneIcon } from "./smartphone.svg?react";
 export { default as Timer } from "./timer.svg";
