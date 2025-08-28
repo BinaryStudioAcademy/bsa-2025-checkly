4ad4cccb65902249ed24d248141074d9b2cf6081
watchlar2000 <sergii.koch@gmail.com>
2025-08-28 20:19:43 +0200

    feat: * update task-update-request dto cy-431

--- packages/shared/src/modules/tasks/libs/types/task-update-request-dto.type.ts
+++ packages/shared/src/modules/tasks/libs/types/task-update-request-dto.type.ts
@@ -1,5 +1,8 @@
+import { type ExecutionTimeTypeValue } from "./execution-time-type.type.js";
+
 type TaskUpdateRequestDto = {
 	description?: string;
+	executionTimeType?: ExecutionTimeTypeValue;
 	title?: string;
 };
 
