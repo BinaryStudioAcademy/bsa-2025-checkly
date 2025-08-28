32dc11a6e71f693a60cf401602ef8a8f210e1ffe
astowner <the1samevl@gmail.com>
2025-08-28 23:54:22 +0300

    fix: remove dirname puppeteerrc cy-202

--- apps/backend/.puppeteerrc.js
+++ apps/backend/.puppeteerrc.js
@@ -4,8 +4,6 @@ const { join } = require("path");
  * @type {import("puppeteer").Configuration}
  */
 
-const __dirname = import.meta.dirname;
-
 module.exports = {
 	cacheDirectory: join(__dirname, ".cache", "puppeteer"),
 };
