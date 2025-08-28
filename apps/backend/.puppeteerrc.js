bad26204c2e06973c7482b78d96f39db3515b1a7
astowner <the1samevl@gmail.com>
2025-08-28 22:35:19 +0300

    feat: revert puppeteer cy-202

--- apps/backend/.puppeteerrc.js
+++ apps/backend/.puppeteerrc.js
@@ -2,7 +2,9 @@ import { join } from "path";
 
 /**
  * @type {import("puppeteer").Configuration}
- */ const __dirname = import.meta.dirname;
+ */
+
+const __dirname = import.meta.dirname;
 
 module.exports = {
 	cacheDirectory: join(__dirname, ".cache", "puppeteer"),
