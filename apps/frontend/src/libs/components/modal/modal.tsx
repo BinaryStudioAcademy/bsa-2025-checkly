90393141feb4238eca23014132ce5306dcdd2128
watchlar2000 <sergii.koch@gmail.com>
2025-08-30 13:33:46 +0200

    feat: * update modal cy-485

--- apps/frontend/src/libs/components/modal/modal.tsx
+++ apps/frontend/src/libs/components/modal/modal.tsx
@@ -46,22 +46,11 @@ const Modal: React.FC<Properties> = ({
 
 	useEffect(() => {
 		const originalOverflow = document.body.style.overflow;
-		const originalPaddingRight = document.body.style.paddingRight;
 
-		if (isOpen) {
-			const scrollbarWidth =
-				window.innerWidth - document.documentElement.clientWidth;
-
-			document.body.style.overflow = "hidden";
-			document.body.style.paddingRight = `${String(scrollbarWidth)}px`;
-		} else {
-			document.body.style.overflow = originalOverflow;
-			document.body.style.paddingRight = originalPaddingRight;
-		}
+		document.body.style.overflow = isOpen ? "hidden" : originalOverflow;
 
 		return (): void => {
 			document.body.style.overflow = originalOverflow;
-			document.body.style.paddingRight = originalPaddingRight;
 		};
 	}, [isOpen]);
 
